ffb3a243f9a32538e95d778068cfea1324170343
RoyHuang922 <yxh089@gmail.com>
2025-06-28 03:00:24 +0000

    changed by roy

--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -9,7 +9,7 @@ int main(){
     int fahrenheit = 100;
     int celsius;
 
-    celsius = fahrenheit;
+    celsius = (static_cast<float>(5) / 9.0) * (fahrenheit - 32);   // (int 5) / (int 9) = 0
 
     std::cout << std::endl;
     std::cout << "Fahrenheit: " << fahrenheit << std::endl;
@@ -19,8 +19,10 @@ int main(){
     
     std::cout << std::endl;
     std::cout << "Float          : " << weight << std::endl;
-    std::cout << "Integer part   : " << weight << std::endl;
-    std::cout << "Fractional part: " << weight << std::endl;
+    std::cout << "Integer part   : " << static_cast<int>(weight) << std::endl;
+    std::cout << "Fractional part: " << (double)((weight - (int)weight) * 10000) << std::endl; 
+    //by converting float  to int, 
+    // all decimal places are removed. no rounding
 
     std::cout << std::endl << std::endl;
     return 0;
