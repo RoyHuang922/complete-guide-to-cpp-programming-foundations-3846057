4ee053bde29db47b9caadcd13947d610784b4cb4
RoyHuang922 <yxh089@gmail.com>
2025-07-05 22:29:00 +0000

    R

--- src/Ch04/04_04b_UsingObjectsWithPointers???/CodeDemo.cpp
+++ src/Ch04/04_04b_UsingObjectsWithPointers???/CodeDemo.cpp
@@ -1,6 +1,7 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 04_04
 // Using Objects with Pointers, by Eduardo Corpeño 
+//如果你认为某些内容用当前资源难以理解，可以尝试其他资源，例如 YouTube
 
 #include <iostream>
 #include <string>
@@ -12,16 +13,19 @@ using TimePoint = std::chrono::time_point<Clock>;
 using Duration = std::chrono::duration<double>;
 
 int main(){
-    TimePoint *start, *end;  //declare two OBJECTS of the TimePoint CLASS (start, end)
-    Duration *elapsed_seconds = new Duration();  //new + the name of the_class(), that is the default constructor
+    TimePoint* start, *end;  //declare two OBJECTS of the TimePoint CLASS (start, end)
+    Duration* elapsed_seconds = new Duration();  //declare, and initiate a pointer with the address
+//Q1    //???????????????????????? the initializer here is an empty address
     //Duration -- class
     //*elapsed_seconds -- pointer that points to the address of the new object in class "Duration"
-    //new Duration() -- new operator + constructor; constructor always have the same name as the class. It returns a pointer to the allocated memory
+    //new Duration() -- new operator + constructor; constructor always have the same name as the class. new Duration() returns a pointer to the allocated memory
 
     std::string input;
 
-    start = new TimePoint(Clock::now());  //instructor also receives an initializer as an gargument
-    
+    start = new TimePoint(Clock::now());  //initiate a declared pointer: ptr = new + constructor(initializer)
+    //constructor also receives an initializer as an gargument
+//Q1    // ??????????????????????? the initializer here is an address with an object
+
     // now() function is a static member function, so no object of the system clock class is required.
 
     //Static member function: A static member function in C++ is a function associated with the class itself, rather than with any specific instance (object) of that class. This means you can call a static member function even if you haven't created any objects of the class. 
@@ -30,11 +34,11 @@ int main(){
     std::cin >> input;
 
     end = new TimePoint(Clock::now());  //fuck you Eduardo
-    *elapsed_seconds = *end - *start;  //WTF?
+    *elapsed_seconds = *end - *start;  //assign object to the empty address "elapsed_seconds"
 
     std::cout << "Reaction time: " << elapsed_seconds->count() << "s" << std::endl; //FUCK
     
-    delete start;
+    delete start;   //free the memory address from the pointer
     delete end;
     delete elapsed_seconds;
 
